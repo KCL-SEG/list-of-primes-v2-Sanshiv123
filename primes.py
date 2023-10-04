d73099821033aeebbcb1a784ee0f64aaaa8a82b7
Sanchusri Kavitha Babu <sanchusri.kavitha_babu@kcl.ac.uk>
2023-10-05 00:25:52 +0100

    solve exercise 1.4

--- primes.py
+++ primes.py
@@ -1,6 +1,15 @@
 """List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes<=0:
+        raise ValueError(f"number of primes = {number_of_primes} should have been a positive non-zero number")
     list = []
-    return list
+    for num in range(1, 500):
+        if num>1:
+            for i in range(2, num):
+                if(num%i)==0:
+                    break
+            else:
+                list.append(num)
+    return list[0:number_of_primes]
+print(f'first 10 prime numbers are {primes(10)}')
